7bab62f53fbea9ea4736f963af105771ee23ed50
SonGohan231 <adambrdon@gmail.com>
2026-08-21 19:53:29 +0200

    refactor: use shared balance math in live battle

--- src/battle/battle_screen_advanced.gd
+++ src/battle/battle_screen_advanced.gd
@@ -44,7 +44,7 @@ func _resolve_player_move(index: int) -> void:
 		_register_move_resonance(move, boost_used)
 		_show_message("%s używa %s. +%d HP%s" % [player_data.name, move.name, healed, "  REZ!" if boost_used else ""], _enemy_turn)
 		return
-	var typed := _typed_damage(move, player_level_value, player_data, enemy_data)
+	var typed := SomadexBattleMath.damage(move, player_level_value, enemy_level_value, player_data, enemy_data, _rng.randf_range(0.92, 1.08))
 	var damage := int(typed.damage)
 	if boost_used:
 		damage = mini(int(round(float(enemy_hp.max_value) * 0.55)), maxi(1, int(round(damage * 1.35))))
@@ -75,7 +75,7 @@ func _enemy_turn() -> void:
 		enemy_hp.value = enemy_current_hp
 		_show_message("%s odnawia %d HP." % [enemy_data.name, healed], func(): _set_mode(Mode.COMMAND))
 		return
-	var typed := _typed_damage(move, enemy_level_value, enemy_data, player_data)
+	var typed := SomadexBattleMath.damage(move, enemy_level_value, player_level_value, enemy_data, player_data, _rng.randf_range(0.92, 1.08))
 	var damage := int(typed.damage)
 	player_current_hp = maxi(0, player_current_hp - damage)
 	_sync_player_hp()
@@ -89,21 +89,6 @@ func _enemy_turn() -> void:
 			_show_message("%s używa %s. Drużyna nie może dalej walczyć." % [enemy_data.name, move.name], _lose_battle)
 	else:
 		_show_message("%s używa %s. -%d HP.%s" % [enemy_data.name, move.name, damage, suffix], func(): _set_mode(Mode.COMMAND))
-
-func _typed_damage(move: Dictionary, level: int, attacker: Dictionary, defender: Dictionary) -> Dictionary:
-	var attack_type := StringName(move.get("type", "KONTAKT"))
-	var attacker_types: Array = attacker.get("types", ["KONTAKT"])
-	var defender_types: Array = defender.get("types", ["KONTAKT"])
-	var effectiveness := SomadexTypeChart.effectiveness(attack_type, defender_types)
-	var total := SomadexTypeChart.total_multiplier(attack_type, attacker_types, defender_types)
-	var attack_stat := maxf(1.0, float(attacker.get("attack", 10)))
-	var defense_stat := maxf(1.0, float(defender.get("defense", 10)))
-	var stat_ratio := clampf(attack_stat / defense_stat, 0.68, 1.42)
-	var base := float(move.get("power", 1)) * (0.70 + maxi(1, level) * 0.038) * stat_ratio
-	var damage := maxi(1, int(round(base * total * _rng.randf_range(0.92, 1.08))))
-	var defender_hp := _max_hp(defender, maxi(1, level))
-	damage = mini(damage, maxi(2, int(round(defender_hp * 0.48))))
-	return {"damage":damage,"effectiveness":effectiveness,"stab":SomadexTypeChart.stab(attack_type, attacker_types),"total":total}
 
 func _close_battle() -> void:
 	if _closing_with_fade || mode == Mode.CLOSED:
